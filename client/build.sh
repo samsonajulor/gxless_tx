2f511899e1f7247af1bb3944a754fbb3a297cfa1
Samson Ajulor <samsonajulor@gmail.com>
2023-10-20 17:46:39 +0100

    fixed build ish

--- client/build.sh
+++ client/build.sh
@@ -4,11 +4,15 @@ rm -rf ./build/html/
 mkdir ./build/html/
 
 rm -rf ../index.html
-rm -rf ../bundle.js
+rm -rf ../index.js
 
-./node_modules/browserify/bin/cmd.js index.js -o  ../doc/bundle.js
+rm -rf ../doc/index.html
+rm -rf ../doc/index.js
+
+./node_modules/browserify/bin/cmd.js index.js -o  ../doc/index.js
+./node_modules/browserify/bin/cmd.js index.js -o  ./build/html/index.js
 
 cp ./index.html ./build/html/
-cp ./index.html ../
+cp ./index.html ../doc
 
 echo "Done building \"./build/html\" at `date`"
