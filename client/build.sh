d5c2801548a85b057b52d4c3477ae5a4065df832
Samson Ajulor <samsonajulor@gmail.com>
2023-10-20 16:52:01 +0100

    done

--- client/build.sh
+++ client/build.sh
@@ -3,11 +3,12 @@
 rm -rf ./build/html/
 mkdir ./build/html/
 
-rm -rf ../doc/
-mkdir ../doc/
+rm -rf ../index.html
+rm -rf ../bundle.js
 
 ./node_modules/browserify/bin/cmd.js index.js -o  ../doc/bundle.js
+
 cp ./index.html ./build/html/
-cp ./index.html ../doc/
+cp ./index.html ../
 
 echo "Done building \"./build/html\" at `date`"
